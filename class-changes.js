d55a1356b34ee62afa7b1675b18a38a20e5b91e6
mushyun1112 <ylee0126@student.monash.edu>
2025-06-12 16:21:47 +1000

    final upload

---
+++ class-changes.js
@@ -0,0 +1,7 @@
+const classChanges = [
+  { time: 4, target: ".popup", add: "active" },
+  { time: 8, target: ".popup", remove: "active" },
+  { time: 16, target: ".popup2", add: "active" },
+  { time: 27, target: ".popup2", remove: "active" },
+
+];
